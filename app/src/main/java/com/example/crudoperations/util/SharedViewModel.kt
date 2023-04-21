a92f294110a863b83e8270a6d0e7df175ce74f1f
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2023-04-21 22:42:26 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/crudoperations/util/SharedViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.crudoperations.util
+
+import android.content.Context
+import android.service.autofill.UserData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SharedViewModel() : ViewModel() {
+    fun saveData(
+        userData: UserData,
+        context: Context
+    ) = CoroutineScope(Dispatchers.IO).launch {
+
+        val fireStoreRef = Firebase.firestore
+            .collection("user")
+            .document(userData.userID)
+
+        try {
+            fireStoreRef.set(userData)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Successfully saved data", Toast.LENGTH_SHORT).show()
+                }
+        } catch (e: Exception) {
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun retrieveData(
+        userID: String,
+        context: Context,
+        data: (UserData) -> Unit
+    ) = CoroutineScope(Dispatchers.IO).launch {
+
+        val fireStoreRef = Firebase.firestore
+            .collection("user")
+            .document(userID)
+
+        try {
+            fireStoreRef.get()
+                .addOnSuccessListener {
+                    // for getting single or particular document
+                    if (it.exists()) {
+                        val userData = it.toObject<UserData>()!!
+                        data(userData)
+                    } else {
+                        Toast.makeText(context, "No User Data Found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } catch (e: Exception) {
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun deleteData(
+        userID: String,
+        context: Context,
+        navController: NavController,
+    ) = CoroutineScope(Dispatchers.IO).launch {
+
+        val fireStoreRef = Firebase.firestore
+            .collection("user")
+            .document(userID)
+
+        try {
+            fireStoreRef.delete()
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Successfully deleted data", Toast.LENGTH_SHORT)
+                        .show()
+                    navController.popBackStack()
+                }
+        } catch (e: Exception) {
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/crudoperations/util/UserData.kt
@@ -0,0 +1,5 @@
+package com.example.crudoperations.util
+
+data class UserData(
+
+)
